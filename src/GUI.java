a85afea4fb486274f959b8e64b4523e00438f229
LMamon <knit-close.0o@icloud.com>
2024-11-12 18:33:05 -0900

    GUI and points.txt added

--- src/GUI.java
+++ src/GUI.java
@@ -2,39 +2,79 @@
 Louis, M.
  */
 
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class GUI extends javafx.scene.layout.Pane {
-    public GUI(ArrayList<Coord> points) {
-        //pane size
-        this.setPrefSize(500, 500);
-        ArrayList<Coord> maximals = new ArrayList<>();
+    ArrayList<Coord> points = new ArrayList<>();
+    ArrayList<Coord> maximals = new ArrayList<>();
 
-        // Iterate through each point
-        for (int i =0; i < points.size(); i++) {
+    //Constructor
+    public GUI(ArrayList<Coord> initialPoints) {
+        this.setPrefSize(500, 500);    //pane size
+        points.addAll(initialPoints);
+        recomputeMaximals();
+    }
 
-            //add circles
-            Coord point = points.get(i);
-            Circle circle = new Circle(point.xCoord(), point.yCoord(), 5);
-            this.getChildren().add(circle);
+    private void mousePoints() {
+        this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton() == MouseButton.PRIMARY) {
+                    //add button
 
-            //compare the current point with other points to determine if maximal
-            for (int j=0 ; j < points.size(); j++) {
-                //skip itself
-                if (i != j){
-                    Coord point2 = points.get(j);
+                    Coord point = new Coord(event.getX(), event.getY());
+                    points.add(point);
+                } else if (event.getButton() == MouseButton.SECONDARY) {
+                    //remove button
+                    Coord nPoint = null;
+                    double minDist = Double.MAX_VALUE;
+                    for (Coord point : points) {
+                        double dist = Math.hypot(point.xCoord() - event.getX(), point.yCoord() - event.getY());
+                        if (dist < minDist) {
+                            minDist = dist;
+                            nPoint = point;
+                        }
+                    }
+                    if (nPoint != null) {
+                        points.remove(nPoint);
+                    }
                 }
-                //if point is maximal add it to list
-                if (point.isLowerLeft(points.get(j))) {
-                    maximals.add(point);
+                recomputeMaximals();
+                updatePane();
+            }
+        });
+    }
+
+
+    private void recomputeMaximals() {
+        maximals.clear();
+        // Iterate through each point
+        Collections.sort(points);
+        for (Coord point : points) {
+            for (Coord otherPoints : points) {
+                if (point != otherPoints && point.isLowerLeft(otherPoints)) {
+                    break;
                 }
             }
+            maximals.add(point);
         }
-        //add lines connecting maximal points
-        for (int l = 0; l < maximals.size(); l++) {
+    }
+
+    private void updatePane() {
+        getChildren().clear();
+        //generate points
+        for (Coord point : points) {
+            Circle circle = new Circle(point.xCoord(), point.yCoord(), 5);
+            getChildren().add(circle);
+        }
+        for (int l = 0; l < maximals.size() -1 ; l++) {
             Coord sLine = maximals.get(l);
             Coord eLine = maximals.get(l+1);
             //add lines to pane
@@ -42,5 +82,6 @@ public class GUI extends javafx.scene.layout.Pane {
             this.getChildren().add(line);
         }
     }
+
 
 }
